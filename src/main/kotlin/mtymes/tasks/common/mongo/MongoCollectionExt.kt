312ff8a4c13780bba9b053c48facdb285543c0af
mtymes <matejtymes@gmail.com>
2022-11-03 10:39:31 +0000

    mtymes: adding upsert function

--- src/main/kotlin/mtymes/tasks/common/mongo/MongoCollectionExt.kt
+++ src/main/kotlin/mtymes/tasks/common/mongo/MongoCollectionExt.kt
@@ -2,6 +2,8 @@ package mtymes.tasks.common.mongo
 
 import com.mongodb.MongoWriteException
 import com.mongodb.client.MongoCollection
+import com.mongodb.client.model.UpdateOptions
+import com.mongodb.client.result.UpdateResult
 import org.bson.conversions.Bson
 
 object MongoCollectionExt {
@@ -21,6 +23,20 @@ object MongoCollectionExt {
         }
     }
 
-    // todo: mtymes - should we fail if has more that one ???
+    // todo: mtymes - should we fail if has more than one ???
     fun <T> MongoCollection<T>.findOne(filter: Bson): T? = find(filter).first()
+
+    fun <T> MongoCollection<T>.upsert(query: Bson, update: Bson): UpdateResult {
+        try {
+            return this.updateOne(query, update, UpdateOptions().upsert(true))
+        } catch (e: MongoWriteException) {
+            // this can happen if two threads try to insert new document with the same id at the same time
+            if (e.error.code == DUPLICATE_CODE) {
+                // retry can easily fix this
+                return this.updateOne(query, update, UpdateOptions().upsert(true))
+            } else {
+                throw e
+            }
+        }
+    }
 }
