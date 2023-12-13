ef993accac6ce5cad00406df7e1cbd8ed1ddc00d
mtymes <matejtymes@gmail.com>
2023-12-13 14:35:02 +0000

    mtymes: using latest javafixes

--- src/main/kotlin/mtymes/tasks/scheduler/dao/UniversalScheduler.kt
+++ src/main/kotlin/mtymes/tasks/scheduler/dao/UniversalScheduler.kt
@@ -26,7 +26,6 @@ import java.time.ZonedDateTime
 import java.util.*
 
 // todo: mtymes - maybe create java version
-// todo: mtymes - define mechanism to add indexes - should be done by users of this class (e.g.: ttl index, unique executionId index, ...)
 // todo: mtymes - cancel task even when it has a running execution
 // todo: mtymes - add ability to have unlimited amount of executions
 // todo: mtymes - is it possible to wait for dependencies somehow ??
